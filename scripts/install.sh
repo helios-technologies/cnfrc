d57bb00af66ed2b451b88b37d1986a3b28322956
Egor Chumack <mafara.intro@gmail.com>
2017-07-24 16:49:05 +0300

    Added install script, awesome config, updated README (#1)

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+cd $HOME
+
+if ! [ -d "$HOME/.cnfrc" ]; then
+  git clone https://github.com/helios-technologies/cnfrc.git .cnfrc
+fi
+
+LIST="xprofile Xresources gitconfig zshrc"
+
+for file in $LIST
+do
+  ln -sf .cnfrc/${file} ".${file}"
+done
+
+sudo pacman --noconfirm -S ttf-dejavu autocutsel dex wmname rxvt-unicode
